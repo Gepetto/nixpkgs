e8c3e80a2addd3ef485980ff072a2ff40fc48a79
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-23 23:31:08 +0200

    gepetto-gui: gepetto-viewer with plugins

--- pkgs/development/python-modules/gepetto-viewer/default.nix
+++ pkgs/development/python-modules/gepetto-viewer/default.nix
@@ -9,65 +9,77 @@
   doxygen,
   jrl-cmakemodules,
   libsForQt5,
+  makeWrapper,
   openscenegraph,
   osgqt,
   pkg-config,
   python-qt,
   qgv,
+  runCommand,
 }:
-buildPythonPackage rec {
-  pname = "gepetto-viewer";
-  version = "5.1.0";
-  pyproject = false; # CMake
+let
+  gepetto-viewer = buildPythonPackage rec {
+    pname = "gepetto-viewer";
+    version = "5.1.0";
+    pyproject = false; # CMake
 
-  src = fetchFromGitHub {
-    owner = "gepetto";
-    repo = "gepetto-viewer";
-    rev = "v${version}";
-    hash = "sha256-A2J3HidG+OHJO8LpLiOEvORxDtViTdeVD85AmKkkOg8=";
+    src = fetchFromGitHub {
+      owner = "gepetto";
+      repo = "gepetto-viewer";
+      rev = "v${version}";
+      hash = "sha256-A2J3HidG+OHJO8LpLiOEvORxDtViTdeVD85AmKkkOg8=";
+    };
+
+    cmakeFlags = [
+      (lib.cmakeBool "BUILD_PY_QCUSTOM_PLOT" (!stdenv.isDarwin))
+      (lib.cmakeBool "BUILD_PY_QGV" (!stdenv.isDarwin))
+    ];
+
+    outputs = [
+      "out"
+      "doc"
+    ];
+
+    buildInputs = [
+      boost
+      python-qt
+      libsForQt5.qtbase
+    ];
+
+    nativeBuildInputs = [
+      cmake
+      doxygen
+      libsForQt5.wrapQtAppsHook
+      pkg-config
+    ] ++ lib.optionals (stdenv.isDarwin && stdenv.isAarch64) [ darwin.autoSignDarwinBinariesHook ];
+
+    propagatedBuildInputs = [
+      jrl-cmakemodules
+      openscenegraph
+      osgqt
+      qgv
+    ];
+
+    doCheck = true;
+
+    # wrapQtAppsHook uses isMachO, which fails to detect binaries without this
+    # ref. https://github.com/NixOS/nixpkgs/pull/138334
+    preFixup = lib.optionalString stdenv.isDarwin "export LC_ALL=C";
+
+    passthru.withPlugins =
+      plugins:
+      runCommand "gepetto-gui" { nativeBuildInputs = [ makeWrapper ]; } ''
+        makeWrapper ${lib.getExe gepetto-viewer} $out/bin/gepetto-gui \
+          --set GEPETTO_GUI_PLUGIN_DIRS ${lib.makeLibraryPath plugins}
+      '';
+
+    meta = {
+      description = "Graphical Interface for Pinocchio and HPP.";
+      homepage = "https://github.com/gepetto/gepetto-viewer";
+      license = lib.licenses.lgpl3Only;
+      maintainers = [ lib.maintainers.nim65s ];
+      mainProgram = "gepetto-gui";
+    };
   };
-
-  cmakeFlags = [
-    (lib.cmakeBool "BUILD_PY_QCUSTOM_PLOT" (!stdenv.isDarwin))
-    (lib.cmakeBool "BUILD_PY_QGV" (!stdenv.isDarwin))
-  ];
-
-  outputs = [
-    "out"
-    "doc"
-  ];
-
-  buildInputs = [
-    boost
-    python-qt
-    libsForQt5.qtbase
-  ];
-
-  nativeBuildInputs = [
-    cmake
-    doxygen
-    libsForQt5.wrapQtAppsHook
-    pkg-config
-  ] ++ lib.optionals (stdenv.isDarwin && stdenv.isAarch64) [ darwin.autoSignDarwinBinariesHook ];
-
-  propagatedBuildInputs = [
-    jrl-cmakemodules
-    openscenegraph
-    osgqt
-    qgv
-  ];
-
-  doCheck = true;
-
-  # wrapQtAppsHook uses isMachO, which fails to detect binaries without this
-  # ref. https://github.com/NixOS/nixpkgs/pull/138334
-  preFixup = lib.optionalString stdenv.isDarwin "export LC_ALL=C";
-
-  meta = {
-    description = "Graphical Interface for Pinocchio and HPP.";
-    homepage = "https://github.com/gepetto/gepetto-viewer";
-    license = lib.licenses.lgpl3Only;
-    maintainers = [ lib.maintainers.nim65s ];
-    mainProgram = "gepetto-gui";
-  };
-}
+in
+gepetto-viewer
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20118,6 +20118,8 @@ with pkgs;
   gecode_6 = qt5.callPackage ../development/libraries/gecode { };
   gecode = gecode_6;
 
+  gepetto-gui = with python3Packages; gepetto-viewer.withPlugins [ gepetto-viewer-corba ];
+
   geph = recurseIntoAttrs (callPackages ../applications/networking/geph { pnpm = pnpm_8; });
 
   gephi = callPackage ../applications/science/misc/gephi { };
